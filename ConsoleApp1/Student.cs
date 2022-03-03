a409a816d8df4f1de00441c18c61b1d4c69b4f48
aviel <aviel@AVIEL-DESKTOP>
2022-03-03 19:00:45 +0200

    Add project files.

---
+++ ConsoleApp1/Student.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Student
+    {
+        string Name;
+        int ID;
+        int Grade;
+        public Student (string Name, int ID, int Grade)
+        {
+            this.Name = Name;
+            this.ID = ID;
+            this.Grade = Grade;
+        }
+    }
+}
